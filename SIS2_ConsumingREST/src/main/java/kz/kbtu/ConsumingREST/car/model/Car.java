6b79f37fc3c1cca61e1181dcda943ab3e85eff7e
bakdaulet <bakdauletcoutinho@gmail.com>
2024-02-05 14:17:24 +0600

    SIUUU

--- SIS2_ConsumingREST/src/main/java/kz/kbtu/ConsumingREST/car/model/Car.java
+++ SIS2_ConsumingREST/src/main/java/kz/kbtu/ConsumingREST/car/model/Car.java
@@ -1,9 +1,11 @@
 package kz.kbtu.ConsumingREST.car.model;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@Data
 public class Car {
 
     private Engine engine;
