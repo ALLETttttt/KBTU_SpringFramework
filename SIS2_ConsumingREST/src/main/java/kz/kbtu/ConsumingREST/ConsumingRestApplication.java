f54a25c4e9b9cb79c45c4e2e6096dcad51dabaf3
bakdaulet <bakdauletcoutinho@gmail.com>
2024-02-09 02:06:02 +0600

    SIUU

--- SIS2_ConsumingREST/src/main/java/kz/kbtu/ConsumingREST/ConsumingRestApplication.java
+++ SIS2_ConsumingREST/src/main/java/kz/kbtu/ConsumingREST/ConsumingRestApplication.java
@@ -1,13 +1,17 @@
 package kz.kbtu.ConsumingREST;
 
 import kz.kbtu.ConsumingREST.person.Person;
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ConsumingRestApplication {
 	private static final String NAME = "John Smith";
+	private static final String NAME_OTHER = "Anna Jones";
 
+	@Test
 	public static void main(String[] args) {
 
 		var context = SpringApplication.run(ConsumingRestApplication.class, args);
@@ -16,10 +20,18 @@ public class ConsumingRestApplication {
 		Person p2 = (Person) context.getBean("personSingleton");
 
 		p1.setName(NAME);
-		System.out.println(NAME.hashCode());
-		System.out.println(p2.getName().hashCode());
+		Assert.assertEquals(NAME, p2.getName());
 
-		System.out.println();
+
+		Person p3 = (Person) context.getBean("personPrototype");
+		Person p4 = (Person) context.getBean("personPrototype");
+
+		p3.setName(NAME);
+		System.out.println(p3.getName().hashCode());
+
+		p4.setName(NAME);
+		System.out.println(p4.getName().hashCode());
+//		System.out.println(p4.getName().hashCode());
 
 		context.close();
 	}
