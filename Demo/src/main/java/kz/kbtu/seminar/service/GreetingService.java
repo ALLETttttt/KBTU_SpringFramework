ab27ce34f805ec4e7f679a6d77286a9db8f92301
bakdaulet <bakdauletcoutinho@gmail.com>
2024-02-17 23:07:53 +0600

    push

--- Demo/src/main/java/kz/kbtu/seminar/service/GreetingService.java
+++ Demo/src/main/java/kz/kbtu/seminar/service/GreetingService.java
@@ -1,6 +1,7 @@
 package kz.kbtu.seminar.service;
 
 import kz.kbtu.seminar.model.Greeting;
+import org.springframework.context.ApplicationEventPublisher;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -8,7 +9,11 @@ public class GreetingService {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    private ApplicationEventPublisher applicationEventPublisher;
+
     public Greeting generateGreeting(String content) {
+        applicationEventPublisher.publishEvent("Greeting hello world!");
+        applicationEventPublisher.publishEvent(123);
         return new Greeting(counter.incrementAndGet(), String.format(template, content));
     }
 }
--- Demo/src/main/java/kz/kbtu/seminar/service/PrinterService.java
+++ Demo/src/main/java/kz/kbtu/seminar/service/PrinterService.java
@@ -3,6 +3,7 @@ package kz.kbtu.seminar.service;
 import kz.kbtu.seminar.model.IPrintable;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -13,5 +14,15 @@ public class PrinterService {
 
     public void print(String text) {
         printer.print(text);
+    }
+
+    @EventListener
+    public void someFunction(String message) {
+        printer.print(message);
+    }
+
+    @EventListener
+    public void someFunction(Integer message) {
+        printer.print(message.toString());
     }
 }
