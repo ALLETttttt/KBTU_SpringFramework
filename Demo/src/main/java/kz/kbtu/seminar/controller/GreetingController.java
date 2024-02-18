1e1ea2970cf09f60a9b66b7e98687555fb8a8352
bakdaulet <bakdauletcoutinho@gmail.com>
2024-02-18 23:07:25 +0600

    push

--- Demo/src/main/java/kz/kbtu/seminar/controller/GreetingController.java
+++ Demo/src/main/java/kz/kbtu/seminar/controller/GreetingController.java
@@ -2,13 +2,15 @@ package kz.kbtu.seminar.controller;
 
 import kz.kbtu.seminar.model.Greeting;
 import kz.kbtu.seminar.service.GreetingService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class GreetingController {
-    GreetingService greetingService = new GreetingService();
+    private final GreetingService greetingService;
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "content", defaultValue = "World!") String content) {
--- Demo/src/main/java/kz/kbtu/seminar/service/GreetingService.java
+++ Demo/src/main/java/kz/kbtu/seminar/service/GreetingService.java
@@ -1,15 +1,18 @@
 package kz.kbtu.seminar.service;
 
 import kz.kbtu.seminar.model.Greeting;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Component;
 
 import java.util.concurrent.atomic.AtomicLong;
-
+@Component
+@RequiredArgsConstructor
 public class GreetingService {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    private ApplicationEventPublisher applicationEventPublisher;
+    private final ApplicationEventPublisher applicationEventPublisher;
 
     public Greeting generateGreeting(String content) {
         applicationEventPublisher.publishEvent("Greeting hello world!");
