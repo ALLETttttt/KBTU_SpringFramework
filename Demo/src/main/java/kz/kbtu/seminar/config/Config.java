b72f844f33a653d9c05d65adc32da6d41e72d4bd
bakdaulet <bakdauletcoutinho@gmail.com>
2024-02-19 15:20:13 +0600

    upd

--- Demo/src/main/java/kz/kbtu/seminar/config/Config.java
+++ Demo/src/main/java/kz/kbtu/seminar/config/Config.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
 @Slf4j
 public class Config {
