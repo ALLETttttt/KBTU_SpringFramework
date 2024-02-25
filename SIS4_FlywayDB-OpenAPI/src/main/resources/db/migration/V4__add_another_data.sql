e6dde4ab95eada9fe813379fec20af40567d78e4
bakdaulet <bakdauletcoutinho@gmail.com>
2024-02-25 15:45:17 +0600

    ura ura ura

---
+++ SIS4_FlywayDB-OpenAPI/src/main/resources/db/migration/V4__add_another_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.employees(id, first_name, last_name, date_of_birth) VALUES (2, 'Alaidar', 'Bakdaulet', '1990-05-15');
+INSERT INTO public.employees(id, first_name, last_name, date_of_birth) VALUES (3, 'Alikhan', 'Didar', '1990-03-15');
+INSERT INTO public.employees(id, first_name, last_name, date_of_birth) VALUES (4, 'Saken', 'Zholbarys', '1999-09-25');
+INSERT INTO public.employees(id, first_name, last_name, date_of_birth) VALUES (5, 'Sabit', 'Nurdaulet', '1002-05-25');
+INSERT INTO public.employees(id, first_name, last_name, date_of_birth) VALUES (6, 'Turganbek', 'Ersultan', '1590-08-11');
+
