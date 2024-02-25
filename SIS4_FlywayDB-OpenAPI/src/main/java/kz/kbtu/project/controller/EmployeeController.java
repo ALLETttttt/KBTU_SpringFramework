84d4a34283eca726335a34c747f99bf04d2d41b9
bakdaulet <bakdauletcoutinho@gmail.com>
2024-02-25 20:44:19 +0600

    done

--- SIS4_FlywayDB-OpenAPI/src/main/java/kz/kbtu/project/controller/EmployeeController.java
+++ SIS4_FlywayDB-OpenAPI/src/main/java/kz/kbtu/project/controller/EmployeeController.java
@@ -1,5 +1,10 @@
 package kz.kbtu.project.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import kz.kbtu.project.model.Employee;
 import kz.kbtu.project.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +26,15 @@ public class EmployeeController {
         return this.employeeRepository.findAll();
     }
 
+    @Operation(summary = "Get a employee by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Employee.class)) }),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Employee not found",
+                    content = @Content) })
     @GetMapping("/employees/{id}")
     public Employee findById(@PathVariable Integer id) throws ChangeSetPersister.NotFoundException {
         return employeeRepository.findById(id)
