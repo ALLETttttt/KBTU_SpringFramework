a72ac2bac2f00eb2e5c8b14a6416f5bf4826d61a
bakdaulet <bakdauletcoutinho@gmail.com>
2024-03-09 00:32:50 +0500

    upd

--- SIS5_Kafka/src/main/java/kz/project/Kafka/listener/MultiTypeKafkaListener.java
+++ SIS5_Kafka/src/main/java/kz/project/Kafka/listener/MultiTypeKafkaListener.java
@@ -7,7 +7,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
-@KafkaListener(id = "multiGroup", topics = "multitype")
+@KafkaListener(id = "multiGroup", topics = "topic")
 public class MultiTypeKafkaListener {
 
     @KafkaHandler
