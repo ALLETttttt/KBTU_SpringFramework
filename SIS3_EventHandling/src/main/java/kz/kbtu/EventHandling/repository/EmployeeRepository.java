7c587672d08c40d3564d202efa8406e4e69c1378
bakdaulet <bakdauletcoutinho@gmail.com>
2024-03-09 15:47:45 +0500

    upd

--- SIS3_EventHandling/src/main/java/kz/kbtu/EventHandling/repository/EmployeeRepository.java
+++ SIS3_EventHandling/src/main/java/kz/kbtu/EventHandling/repository/EmployeeRepository.java
@@ -3,5 +3,10 @@ package kz.kbtu.EventHandling.repository;
 import kz.kbtu.EventHandling.model.Employee;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends CrudRepository<Employee, Integer> {
+
+
+    Optional<Employee> findEmployeeById(Integer id);
 }
