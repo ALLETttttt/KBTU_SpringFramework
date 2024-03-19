545d93e99e65db7c6ff7e2d23405b7f333c0dd29
bakdaulet <bakdauletcoutinho@gmail.com>
2024-03-19 12:33:34 +0500

    push

--- SIS4_FlywayDB-OpenAPI/src/main/resources/db/migration/V1__init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS products(
-   id UUID PRIMARY KEY,
-   product_name  VARCHAR(255) NOT NULL,
-   stock NUMERIC NOT NULL default 0,
-   manufacturer VARCHAR(255) NOT NULL
-);
--- SIS4_FlywayDB-OpenAPI/src/main/resources/db/migration/V2__created_on_column.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE products ADD COLUMN created_on TIMESTAMP NOT NULL default current_timestamp;
