854200097e827d6ff7b057b091912561178c2511
bakdaulet <bakdauletcoutinho@gmail.com>
2024-04-14 18:59:47 +0500

    push

--- SIS7_SoapWebService/src/main/java/kz/project/SoapWebService/SoapWebServiceApplication.java
+++ SIS7_SoapWebService/src/main/java/kz/project/SoapWebService/SoapWebServiceApplication.java
@@ -2,10 +2,16 @@ package kz.project.SoapWebService;
 
 import io.spring.guides.gs_producing_web_service.GetCountryResponse;
 import kz.project.SoapWebService.country.CountryClient;
+import kz.project.SoapWebService.number.NumberClient;
+import kz.project.SoapWebService.wsdl.NumberToDollarsResponse;
+import kz.project.SoapWebService.wsdl.NumberToWordsResponse;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
 
 @SpringBootApplication
 public class SoapWebServiceApplication {
@@ -16,7 +22,7 @@ public class SoapWebServiceApplication {
 
 
 	@Bean
-	CommandLineRunner lookup(CountryClient countryClient) {
+	CommandLineRunner lookup(CountryClient countryClient, NumberClient numberClient) {
 		return args -> {
 			String country = "Spain";
 
@@ -25,6 +31,15 @@ public class SoapWebServiceApplication {
 			}
 			GetCountryResponse response = countryClient.getCountry(country);
 			System.err.println(response.getCountry().getCurrency());
+
+			BigDecimal num = new BigDecimal("4000");
+			NumberToDollarsResponse response1 = numberClient.getDollar(num);
+			System.err.println(response1.getNumberToDollarsResult());
+
+			BigInteger number = new BigInteger("300000");
+			NumberToWordsResponse response2 = numberClient.getWord(number);
+			System.err.println(response2.getNumberToWordsResult());
+
 		};
 	}
 }
