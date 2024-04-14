a7b143b569d27b29bd8820d868919d34fe634ffb
bakdaulet <bakdauletcoutinho@gmail.com>
2024-04-14 17:47:23 +0500

    push

--- SIS7_SoapWebService/src/main/java/kz/project/SoapWebService/SoapWebServiceApplication.java
+++ SIS7_SoapWebService/src/main/java/kz/project/SoapWebService/SoapWebServiceApplication.java
@@ -1,7 +1,11 @@
 package kz.project.SoapWebService;
 
+import io.spring.guides.gs_producing_web_service.GetCountryResponse;
+import kz.project.SoapWebService.country.CountryClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SoapWebServiceApplication {
@@ -10,4 +14,17 @@ public class SoapWebServiceApplication {
 		SpringApplication.run(SoapWebServiceApplication.class, args);
 	}
 
+
+	@Bean
+	CommandLineRunner lookup(CountryClient countryClient) {
+		return args -> {
+			String country = "Spain";
+
+			if (args.length > 0) {
+				country = args[0];
+			}
+			GetCountryResponse response = countryClient.getCountry(country);
+			System.err.println(response.getCountry().getCurrency());
+		};
+	}
 }
