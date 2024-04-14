a0f7cf50ab88adb29dac29ec5ba6d7452fe30c2f
bakdaulet <bakdauletcoutinho@gmail.com>
2024-04-14 17:37:06 +0500

    push

---
+++ SIS7_SoapWebService/src/main/java/kz/project/SoapWebService/SoapWebServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.project.SoapWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapWebServiceApplication.class, args);
+	}
+
+}
