0239c31efa98eaf60688b40e4b316bfbe21de85c
bakdaulet <bakdauletcoutinho@gmail.com>
2024-04-28 15:36:57 +0500

    push

--- gatling-maven/target/gatling/orderdbsimulation-20240428103615712/js/stats.js
+++ gatling-maven/target/gatling/orderdbsimulation-20240428103615712/js/stats.js
@@ -11,23 +11,23 @@ stats: {
         "ko": "0"
     },
     "minResponseTime": {
-        "total": "3",
-        "ok": "3",
+        "total": "7",
+        "ok": "7",
         "ko": "-"
     },
     "maxResponseTime": {
-        "total": "113",
-        "ok": "113",
+        "total": "172",
+        "ok": "172",
         "ko": "-"
     },
     "meanResponseTime": {
-        "total": "15",
-        "ok": "15",
+        "total": "23",
+        "ok": "23",
         "ko": "-"
     },
     "standardDeviation": {
-        "total": "24",
-        "ok": "24",
+        "total": "42",
+        "ok": "42",
         "ko": "-"
     },
     "percentiles1": {
@@ -41,13 +41,13 @@ stats: {
         "ko": "-"
     },
     "percentiles3": {
-        "total": "42",
-        "ok": "42",
+        "total": "122",
+        "ok": "122",
         "ko": "-"
     },
     "percentiles4": {
-        "total": "99",
-        "ok": "99",
+        "total": "162",
+        "ok": "162",
         "ko": "-"
     },
     "group1": {
@@ -81,13 +81,13 @@ stats: {
     }
 },
 contents: {
-"req_get-all-address-0d0fb": {
+"req_get-all-orders-40ab8": {
         type: "REQUEST",
-        name: "Get All Addresses",
-path: "Get All Addresses",
-pathFormatted: "req_get-all-address-0d0fb",
+        name: "Get All Orders",
+path: "Get All Orders",
+pathFormatted: "req_get-all-orders-40ab8",
 stats: {
-    "name": "Get All Addresses",
+    "name": "Get All Orders",
     "numberOfRequests": {
         "total": "10",
         "ok": "10",
@@ -99,38 +99,38 @@ stats: {
         "ko": "-"
     },
     "maxResponseTime": {
-        "total": "113",
-        "ok": "113",
+        "total": "119",
+        "ok": "119",
         "ko": "-"
     },
     "meanResponseTime": {
-        "total": "21",
-        "ok": "21",
+        "total": "20",
+        "ok": "20",
         "ko": "-"
     },
     "standardDeviation": {
-        "total": "31",
-        "ok": "31",
+        "total": "33",
+        "ok": "33",
         "ko": "-"
     },
     "percentiles1": {
-        "total": "12",
-        "ok": "12",
+        "total": "10",
+        "ok": "10",
         "ko": "-"
     },
     "percentiles2": {
-        "total": "12",
-        "ok": "12",
+        "total": "10",
+        "ok": "10",
         "ko": "-"
     },
     "percentiles3": {
-        "total": "68",
-        "ok": "68",
+        "total": "71",
+        "ok": "71",
         "ko": "-"
     },
     "percentiles4": {
-        "total": "104",
-        "ok": "104",
+        "total": "109",
+        "ok": "109",
         "ko": "-"
     },
     "group1": {
@@ -163,56 +163,56 @@ stats: {
         "ko": "-"
     }
 }
-    },"req_get-address-by--dc6ba": {
+    },"req_get-order-detai-d14d5": {
         type: "REQUEST",
-        name: "Get Address by postal code",
-path: "Get Address by postal code",
-pathFormatted: "req_get-address-by--dc6ba",
+        name: "Get Order Detail",
+path: "Get Order Detail",
+pathFormatted: "req_get-order-detai-d14d5",
 stats: {
-    "name": "Get Address by postal code",
+    "name": "Get Order Detail",
     "numberOfRequests": {
         "total": "10",
         "ok": "10",
         "ko": "0"
     },
     "minResponseTime": {
-        "total": "3",
-        "ok": "3",
+        "total": "7",
+        "ok": "7",
         "ko": "-"
     },
     "maxResponseTime": {
-        "total": "38",
-        "ok": "38",
+        "total": "172",
+        "ok": "172",
         "ko": "-"
     },
     "meanResponseTime": {
-        "total": "9",
-        "ok": "9",
+        "total": "27",
+        "ok": "27",
         "ko": "-"
     },
     "standardDeviation": {
-        "total": "10",
-        "ok": "10",
+        "total": "49",
+        "ok": "49",
         "ko": "-"
     },
     "percentiles1": {
-        "total": "6",
-        "ok": "6",
+        "total": "12",
+        "ok": "12",
         "ko": "-"
     },
     "percentiles2": {
-        "total": "9",
-        "ok": "9",
+        "total": "14",
+        "ok": "14",
         "ko": "-"
     },
     "percentiles3": {
-        "total": "26",
-        "ok": "26",
+        "total": "101",
+        "ok": "101",
         "ko": "-"
     },
     "percentiles4": {
-        "total": "36",
-        "ok": "36",
+        "total": "158",
+        "ok": "158",
         "ko": "-"
     },
     "group1": {
